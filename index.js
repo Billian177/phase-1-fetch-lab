9b262471b1cfeb832320b72c89cdeb4aad179be0
bill apiyo <billian177@gmail.com>
2022-06-16 18:07:04 +0300

    commit tree 503e75180894016d062e79ed1af1555a3e084af1

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const { returns } = require("chai-spies");
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function (resp){
+      return resp.json();
+    })
+
+    .then(function (json){
+      return renderBooks(json);
+    });
 }
 
 function renderBooks(books) {
